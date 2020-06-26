0d97732f759885917cb37fef9a4b8e43def95522
danilaslynko <danilaslynko@yandex.ru>
2020-06-26 18:02:47 +0300

    Deploy v6

--- src/main/java/com/studentcloud/dbaccess/config/FlywayConfig.java
+++ src/main/java/com/studentcloud/dbaccess/config/FlywayConfig.java
@@ -1,11 +1,19 @@
 package com.studentcloud.dbaccess.config;
 
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FlywayConfig {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceURL;
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
 
     @Bean(initMethod = "migrate")
     public Flyway flyway() {
@@ -13,9 +21,9 @@ public class FlywayConfig {
         flyway.setBaselineOnMigrate(true);
         flyway.setLocations("classpath:\\migration");
         flyway.setDataSource(
-                "jdbc:mysql://localhost:3306/studcloud_db?useUnicode=true&serverTimezone=UTC",
-                "root",
-                "1Qwerty1");
+                datasourceURL,
+                datasourceUsername,
+                datasourcePassword);
         return flyway;
     }
 }
